e2da302f0f050640e6c57335f22f596b65372b65
andrevvt7 <andrevvt7@gmail.com>
2022-03-31 18:57:35 -0300

    primer commit

---
+++ tp2_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 5
+#define M 7
+
+int main(){
+    
+    srand((int)time(NULL));
+
+    int i,j;
+    int mt[N][M];
+    int *pmt = &mt[0][0];
+
+
+    for(i = 0; i < N; i++){
+        for(j = 0; j < M; j++){
+            *pmt = 1 + rand() % 100;
+            printf("%d\t", *pmt);
+        }
+        printf("\n");
+    }
+        printf("\n");
+
+return 0;
+}
